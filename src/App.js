758bdd4d8299a4e4e83c66dbf207ff8d4d6d4874
sgbo5003 <sgbo5003@naver.com>
2020-09-18 12:05:14 +0900

    Edit 03장

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
 
+function Food({ fav }) {
+  return <h1>I love {fav}</h1>;
+}
+
 function App() {
   //return <div className="App" />;
   return (
     <div>
       <h1>Hello!!!!!</h1>
+      <Food fav="김치" />
+      <Food fav="짜장면" />
+      <Food fav="짬뽕" />
+      <Food fav="만두" />
+      <Food fav="피자" />
     </div>
   );
 }
