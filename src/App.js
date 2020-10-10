54b351efa0d8e02dee51b6a2ebbedfe654d9d9fe
sgbo5003 <sgbo5003@naver.com>
2020-10-10 18:35:53 +0900

    1007 학습내용

--- src/App.js
+++ src/App.js
@@ -1,66 +1,30 @@
 import React from "react";
-import img01 from "./images/1.jpg";
-import img02 from "./images/2.jpg";
-import img03 from "./images/3.jpg";
-import img04 from "./images/4.jpg";
-import img05 from "./images/5.jpg";
 
-function Food({ name, picture, alt0 }) {
-  return (
-    <div>
-      <h2>I love {name}</h2>
-      <img src={picture} alt={alt0} />
-    </div>
-  );
+class App extends React.Component{
+  state = {
+    count: 0,
+  };
+
+  add = () => {
+    this.setState(current => ({
+      count: current.count + 1
+    }));
+  }
+
+  minus = () => {
+    this.setState(current => ({
+      count: current.count + -1
+    }));
+  }
+
+  render(){
+    return (
+      <div>
+        <h1>The number is: {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>&nbsp;&nbsp;&nbsp;
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
-
-const foodILike = [
-  {
-    id: 1,
-    name: "치킨",
-    image: img01,
-    message: "치킨 사진",
-  },
-  {
-    id: 2,
-    name: "닭발",
-    image: img02,
-    message: "닭발 사진",
-  },
-  {
-    id: 3,
-    name: "만두",
-    image: img03,
-    message: "만두 사진",
-  },
-  {
-    id: 4,
-    name: "피자",
-    image: img04,
-    message: "피자 사진",
-  },
-  {
-    id: 5,
-    name: "떡볶이",
-    image: img05,
-    message: "떡볶이 사진",
-  },
-];
-
-function App() {
-  //return <div className="App" />;
-  return (
-    <div>
-      {foodILike.map((dish) => (
-        <Food
-          key={dish.id}
-          name={dish.name}
-          picture={dish.image}
-          alt0={dish.message}
-        />
-      ))}
-    </div>
-  );
-}
-
 export default App;
