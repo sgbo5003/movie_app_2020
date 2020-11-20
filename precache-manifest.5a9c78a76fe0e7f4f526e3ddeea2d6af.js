14097d0b23983eca18dca9f76bc41c1855c406b1
sgbo5003 <sgbo5003@naver.com>
2020-11-20 12:39:33 +0900

    Updates

---
+++ precache-manifest.5a9c78a76fe0e7f4f526e3ddeea2d6af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6b37285cdad2ae3b669420f0b13803a",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "77faf886d1ae6a26edc4",
+    "url": "/movie_app_2020/static/css/main.7664ce6b.chunk.css"
+  },
+  {
+    "revision": "ca1290ec655463a20b0e",
+    "url": "/movie_app_2020/static/js/2.ad030fd9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.ad030fd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77faf886d1ae6a26edc4",
+    "url": "/movie_app_2020/static/js/main.915682f6.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
